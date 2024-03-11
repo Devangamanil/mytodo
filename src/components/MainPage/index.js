a073d25896e7b27dd53ff1b87390331daa38f7c5
Devangamanil <devangamanilkumar72@gmail.com>
2024-03-11 16:54:35 +0530

    first commit

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -55,7 +55,7 @@ function TodoApp() {
 
   return (
     <div className="container">
-      <h1>Todo App</h1>
+      <h1>Day Goals!</h1>
       <div className="input-container">
         <input
           type="text"
